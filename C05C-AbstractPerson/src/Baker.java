2594709fe3de86cac0f9a0c5485dd8aa2cecd1a7
alexvoneida <alexvoneida@gmail.com>
2026-01-29 15:12:48 -0700

    Baker child class: we both contributed equitably

---
+++ C05C-AbstractPerson/src/Baker.java
@@ -0,0 +1,10 @@
+
+public class Baker extends Person {
+	void askQuestion() {
+		System.out.println("What do you like to do?");
+	}
+	void answerQuestion() {
+		System.out.println("My favorite pie is pumpkin");
+	}
+}
+
